f835e30daa6724ac5a3c65523e1f16c9f637e2f2
KINOX0924 <kinox1010@gmail.com>
2025-05-14 15:28:03 +0900

    Commit all changes

--- Lecture/0514.py
+++ Lecture/0514.py
@@ -135,19 +135,22 @@ import re
 # if result != None :
 #     phone1 = result.group(1)
 
-contents = """
-           우리커피숍 100-90-12345
-           영풍문고 101-91-12121
-           영미청과 102-92-23451
-           황금코인 103-89-13579
-           우리문구 104-91-24689
-           옆집회사 105-82-12345
-           """
+# contents = """
+#            우리커피숍 100-90-12345
+#            영풍문고 101-91-12121
+#            영미청과 102-92-23451
+#            황금코인 103-89-13579
+#            우리문구 104-91-24689
+#            옆집회사 105-82-12345
+#            """
 
-pattern = r"(\d{3})-(\d{2})-(\d{5})"
-regex   = re.compile(pattern)
-result  = regex.finditer(contents)
+# pattern = r"(\d{3})-(\d{2})-(\d{5})"
+# regex   = re.compile(pattern)
+# result  = regex.finditer(contents)
 
-for item in result :
-    if int(item.group(2)) >= 90 and int(item.group(2)) <= 99 :
-        print(item.group(1) + "-" + item.group(2) + "-" + item.group(3))
+# for item in result :
+#     if int(item.group(2)) >= 90 and int(item.group(2)) <= 99 :
+#         print(item.group(1) + "-" + item.group(2) + "-" + item.group(3))
+
+
+# MySQL 다운로드
