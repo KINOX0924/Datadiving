fef2bd15d68878f1e51ea53bc22cafeac5e95f4e
KINOX0924 <kinox1010@gmail.com>
2025-05-14 16:07:04 +0900

    Commit all changes

--- Practice/Bank Program/PatternList.py
+++ Practice/Bank Program/PatternList.py
@@ -1,19 +1,22 @@
 # 주로 사용하는 패턴을 모아두고 그것으로 검증 진행하는 함수들의 모음
 import re
 
-# //FIXME [1] 관리자 모듈 수정 작업 전에 이것 먼저 수정 - 패턴 설정 필요
+# //FIXME [1] 관리자 모듈 수정 작업 전에 이것 먼저 수정 - 패턴 설정 필요 //TODO [1] 수정 완료
 def checkPhoneNumber(phone_number) :
     phonenumber_pattern = r"^\d{3}-\d{3,4}-\d{4}\b"
-    search_phonenumber = pattern.finditer(phone_number)
+    phonenumber = re.finditer(phonenumber_pattern , phone_number)
     
-    for item in search_phonenumber :
-        print(item.group())
+    search_phonenumber = ""
     
-    if search_phonenumber == None :
+    for value in phonenumber :
+        search_phonenumber = value.group()
+    
+    if phonenumber == None :
         return None
     else :
         return search_phonenumber
 
 
 if __name__ == "__main__" :
-    number = checkPhoneNumber("010-1155-5511")
+    number = checkPhoneNumber("010-1155-55111")
+    print(number)
