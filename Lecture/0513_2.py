721e1d650b81d964453a73e778ee2e0505e99464
KINOX <kinox1010@gmail.com>
2025-05-18 01:48:24 +0900

    Commit all changes

--- Lecture/0513_2.py
+++
@@ -1,3 +0,0 @@
-from PackagePrac.mod2 import isEven , toUpper
-
-# 패키지는 물리적 디렉토리를 말함
--- Weekpay Calculator/__init__.py
+++
@@ -1,5 +0,0 @@
-#프로젝트 시작 파일명(주로 사용되는) : __init__
-from weekpayManager import Paymanager
-
-mgr = Paymanager()
-mgr.start()
