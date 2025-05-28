f03949ad6c01973e0f7f363c1b6d15620ecdc8d6
KINOX <kinox1010@gmail.com>
2025-05-28 09:21:45 +0900

    Low level Algorithm practice file commit

---
+++ Practice/Algorithm/Question0003.py
@@ -0,0 +1,28 @@
+# 10 개의 숫자를 리스트로 받아서, 각 숫자가 리스트 안에서 몇 번 나타나는지(빈도수) 를 Dict 타입으로 만들어서 출력하는 알고리즘
+
+def insertNumber() :
+    number_list = []
+    
+    while len(number_list) < 10 :
+        try :
+            number = int(input("숫자 입력 : "))
+            number_list.append(number)
+        except ValueError :
+            print("숫자만 입력")
+    
+    return number_list
+
+def countNumber(number_list) :
+    count_number_list = {}
+    
+    for number in number_list :
+        if str(number) in count_number_list :
+            count_number_list[str(number)] += 1
+        else :
+            count_number_list[str(number)] = 1
+        print(count_number_list)
+
+
+if __name__ == "__main__" :
+    number_list = insertNumber()
+    countNumber(number_list)
