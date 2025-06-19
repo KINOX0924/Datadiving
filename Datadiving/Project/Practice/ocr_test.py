a62ad37e7c2fc9a42c50def6e987a2ec362a3950
KINOX <kinox1010@gmail.com>
2025-06-19 18:04:09 +0900

    commit text py file

--- Datadiving/Project/Practice/ocr_test.py
+++ Datadiving/Project/Practice/ocr_test.py
@@ -1,27 +1,17 @@
 import cv2
-import numpy as np
-import pytesseract
+import easyocr
 
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+img_path = r"..\Test_Image\receipt_3.jpg"
+image    = cv2.imread(img_path)
 
-image     = cv2.imread("../Test_Image/receipt_3.jpg")
-hsv_image = cv2.cvtColor(image , cv2.COLOR_BGR2HSV)
+gray = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
+thresh = cv2.adaptiveThreshold(gray , 255 , cv2.ADAPTIVE_THRESH_GAUSSIAN_C , cv2.THRESH_BINARY , 15 , 8)
 
-lower_blue = np.array([90 , 50 , 50])
-upper_blue = np.array([130 , 255 , 255])
-blue_mask  = cv2.inRange(hsv_image , lower_blue , upper_blue)
-blue_ratio = np.sum(blue_mask > 0) / (image.shape[0] * image.shape[1])
+cv2.imwrite("gray.jpg" , gray)
+cv2.imwrite("thresh.jpg" , thresh)
 
-try :
-    gray = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
-except :
-    gray = image.copy()
+reader  = easyocr.Reader(["ko" , "en"])
+results = reader.readtext(thresh , detail = 0)
 
-if blue_ratio > 0.03 :
-    processed = cv2.adaptiveThreshold(gray , 255 , cv2.ADAPTIVE_THRESH_GAUSSIAN_C , cv2.THRESH_BINARY , 15 , 8)
-else :
-    processed = cv2.threshold(gray , 150 , 255 , cv2.THRESH_BINARY_INV)
-
-text = pytesseract.image_to_string(processed , lang="kor+eng")
-
-print(text)
+for line in results :
+    print(line)
