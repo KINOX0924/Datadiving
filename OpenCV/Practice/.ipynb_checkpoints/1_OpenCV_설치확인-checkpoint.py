154bdc4dbc99ceb4ca85a93f97120accf801f283
KINOX <kinox1010@gmail.com>
2025-07-17 21:00:44 +0900

    CUDA TEST COMPLETE commit

--- OpenCV/Practice/.ipynb_checkpoints/1_OpenCV_설치확인-checkpoint.py
+++
@@ -1,4 +0,0 @@
-import cv2
-# 파이썬에서 OpenCV 를 사용하기 위한 라이브러리를 불러옵니다.
-
-print(f"Hello OpenCV : {cv2.__version__}")
--- OpenCV/Practice/2_OpenCV_BMP_영상출력.py
+++ OpenCV/Practice/2_OpenCV_BMP_영상출력.py
@@ -1,4 +1,10 @@
 import cv2
-import os
 
-img = cv2.imread("")
+img = cv2.imread("./images/Lena.bmp")   # 이미지의 경로를 가져옴
+if img is None :                                      # 이미지 경로를 읽지 못 해서 이미지를 못 읽어 올 경우
+    print("이미지 경로가 잘못 설정되었거나 이미지가 없습니다.") # 왼쪽과 같은 문구를 출력
+else :
+
+    cv2.imshow("image" , img)   # 이미지를 제대로 읽었다면
+    cv2.waitKey(0)              # 아무 키나 눌릴때까지 기다리고
+    cv2.destroyAllWindows()     # 키가 눌리면 열렸던 모든 창을 닫음
